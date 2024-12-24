13d01fe5be8c8d9b5ab4f5041c2e0b2c5b366c8b
Jollypru <Jollyprumarma2020@gmail.com>
2024-12-24 17:33:55 +0600

    delete operation added

--- src/Pages/MyProfile/MyArtifacts.jsx
+++ src/Pages/MyProfile/MyArtifacts.jsx
@@ -1,26 +1,60 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../providers/AuthProvider';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const MyArtifacts = () => {
     const { user } = useContext(AuthContext);
     const [myArtifacts, setMyArtifacts] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         fetch(`http://localhost:5000/myAddedArtifacts?email=${user.email}`)
             .then(res => res.json())
             .then(data => {
-                console.log(data);
+                // console.log(data);
                 setMyArtifacts(data);
             })
     }, [user.email])
 
-    // const handleDelete = (_id) => {
-    //     fetch(`http://localhost:5000/artifacts/${_id}`,{
-    //         method: 'DELETE'
-    //     })
+    const handleDelete = (_id) => {
 
-    // }
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        })
+        .then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:5000/artifacts/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            setMyArtifacts(remaining => remaining.filter(artifact => artifact._id !== _id));
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "The artifact has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                        navigate('/all-Artifacts');
+                    })
+            }
+        });
+
+
+
+    }
+
+
 
     return (
         <div className="overflow-x-auto">
@@ -49,7 +83,7 @@ const MyArtifacts = () => {
                                         <td>
                                             <NavLink to={`/artifactDetails/${artifact._id}`}><button className='bg-green-500 py-1 px-4 rounded-md text-white mr-5'>View</button></NavLink>
                                             <NavLink to={`/updateArtifact/${artifact._id}`}><button className='bg-blue-500 py-1 px-4 rounded-md text-white mr-5'>Update</button></NavLink>
-                                            <button className='bg-red-600 py-1 px-4 rounded-md text-white'>Delete</button>
+                                            <button onClick={() => handleDelete(artifact._id)} className='bg-red-600 py-1 px-4 rounded-md text-white'>Delete</button>
                                         </td>
                                     </tr>)
                                 }
